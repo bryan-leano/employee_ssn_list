a637fd72270aab202a2d8040b0bb1f3c0bd4a966
Bryan Leanoe <bryan.leano@gmail.com>
2019-11-19 01:38:52 -0700

    added user input method

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        
+        Scanner sc = new Scanner(System.in);
+
+        String[] employeeNames = new String[5];
+        int[] socialSecurityNumbers = new int[5];
+
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("What is your employee name?");
+            employeeNames[i] = sc.nextLine();
+            System.out.println("What is their SSN?");
+            socialSecurityNumbers[i] = sc.nextInt();
+            sc.nextLine();
+        };
+
+        for (int i = 0; i < employeeNames.length; i++) {
+            System.out.println("Employee's name is " + employeeNames[i]);
+            System.out.println("And " + employeeNames[i] + "'s SSN is " + socialSecurityNumbers[i]);
+        }
 
     }
 }
